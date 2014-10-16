de6738ef5c55c781ea858166be868aa55136e2b3
Tianchi Liu <ltcwendell@gmail.com>
2014-10-15 20:06:38 -0700

    Added layout for main screen.

--- app/src/main/java/com/uw/hcde/fizzlab/trace/main/MainActivity.java
+++ app/src/main/java/com/uw/hcde/fizzlab/trace/main/MainActivity.java
@@ -1,7 +1,9 @@
-package com.uw.hcde.fizzlab.trace.module;
+package com.uw.hcde.fizzlab.trace.main;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.uw.hcde.fizzlab.trace.R;
 
@@ -14,6 +16,9 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
 }

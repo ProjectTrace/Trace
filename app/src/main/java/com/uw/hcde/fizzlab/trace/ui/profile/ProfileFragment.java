84aa2a3b20041ffade7e33bd5d50b8569f20a54c
Tianchi Liu <ltcwendell@gmail.com>
2015-02-11 21:48:09 -0800

    Added logout button

--- app/src/main/java/com/uw/hcde/fizzlab/trace/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/uw/hcde/fizzlab/trace/ui/profile/ProfileFragment.java
@@ -2,13 +2,19 @@ package com.uw.hcde.fizzlab.trace.ui.profile;
 
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v13.app.FragmentTabHost;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.parse.ParseUser;
 import com.uw.hcde.fizzlab.trace.R;
+import com.uw.hcde.fizzlab.trace.database.ParseConstant;
+import com.uw.hcde.fizzlab.trace.main.DispatchActivity;
+import com.uw.hcde.fizzlab.trace.ui.BaseActivity;
 
 /**
  * Profile fragment.
@@ -26,26 +32,26 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
-//        // Sets navigation title
-//        ((BaseActivity) getActivity()).setNavigationTitle(R.string.profile);
-//
-//        // Sets displayed username
-//        TextView userName = (TextView) view.findViewById(R.id.text_username);
-//        userName.setText(ParseUser.getCurrentUser().getUsername());
-//
-//        // Sets up the log out button click handler
-//        View buttonLogout = view.findViewById(R.id.button_logout);
-//        buttonLogout.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                // Calls the Parse log out method
-//                ParseUser.logOut();
-//
-//                // Clears root activity and switch to dispatch activity
-//                Intent intent = new Intent(getActivity(), DispatchActivity.class);
-//                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-//                startActivity(intent);
-//            }
-//        });
+        // Sets navigation title
+        ((BaseActivity) getActivity()).setNavigationTitle(R.string.profile);
+
+        // Sets displayed username
+        TextView name = (TextView) view.findViewById(R.id.profile_name);
+        name.setText(ParseUser.getCurrentUser().getString(ParseConstant.KEY_FULL_NAME));
+
+        // Sets up the log out button click handler
+        View buttonLogout = view.findViewById(R.id.profile_log_out);
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Calls the Parse log out method
+                ParseUser.logOut();
+
+                // Clears root activity and switch to dispatch activity
+                Intent intent = new Intent(getActivity(), DispatchActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
 
         mTabHost = (FragmentTabHost) view.findViewById(R.id.tab_host);
         mTabHost.setup(getActivity(), getActivity().getFragmentManager(), R.id.tab_content);
